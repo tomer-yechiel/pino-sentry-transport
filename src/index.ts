e66f58eef90fc8f2d93a7489e125ba898932832e
Tomer Yechiel <tomery@metisdata.io>
2022-06-14 23:11:28 +0300

    fix formatting

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import Sentry from '@sentry/node';
-import build from 'pino-abstract-transport'
+import Sentry from "@sentry/node";
+import build from "pino-abstract-transport";
 
 export default async function () {
   Sentry.init({
-    dsn: 'https://300e099cd83d4e6a8656272d2b9d54ee@o1173646.ingest.sentry.io/6505195',
+    dsn: "https://300e099cd83d4e6a8656272d2b9d54ee@o1173646.ingest.sentry.io/6505195",
     // ...
   });
 
-  return build(async function (source) {
-    for await (const obj of source) {
-      Sentry.captureException(obj);
+  return build(
+    async function (source) {
+      for await (const obj of source) {
+        Sentry.captureException(obj);
+      }
+    },
+    {
+      async close(err) {
+        await Sentry.flush();
+      },
     }
-  }, {
-    async close (err) {
-      await Sentry.flush()
-    }
-  })
+  );
 }
