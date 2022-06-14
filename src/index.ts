be579f85a0a30d9edc42a506454ed39d3a387b9b
Tomer Yechiel <tomery@metisdata.io>
2022-06-14 23:09:22 +0300

    initial work

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-export function foo(a: number, b: number): number {
-  return a + b;
-}
+import Sentry from '@sentry/node';
+import build from 'pino-abstract-transport'
 
-export function bar(a: number, b: number): number {
-  return a - b;
+export default async function () {
+  Sentry.init({
+    dsn: 'https://300e099cd83d4e6a8656272d2b9d54ee@o1173646.ingest.sentry.io/6505195',
+    // ...
+  });
+
+  return build(async function (source) {
+    for await (const obj of source) {
+      Sentry.captureException(obj);
+    }
+  }, {
+    async close (err) {
+      await Sentry.flush()
+    }
+  })
 }
